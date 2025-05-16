ca2609aaa8e12ec39b9262696b55ee8926bb559e
Fabriciovics <fabriciovics@gmail.com>
2025-05-16 09:15:09 -0300

    :tada: Commit Inicial Projeto Blog Pessoal

---
+++ src/test/java/com/genation/blogpessoal/BlogpessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genation.blogpessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
