39305756a2aa077f4bfca7bbc108fa9d57bf3347
Fabriciovics <fabriciovics@gmail.com>
2025-05-26 18:00:54 -0300

    Implementacao do teste unitario.

--- src/main/java/com/genation/blogpessoal/security/UserDetailsServiceImpl.java
+++ src/main/java/com/genation/blogpessoal/security/UserDetailsServiceImpl.java
@@ -4,7 +4,6 @@ import com.genation.blogpessoal.repository.UsuarioRepository;
 
 import java.util.Optional;
 
-import com.genation.blogpessoal.security.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
