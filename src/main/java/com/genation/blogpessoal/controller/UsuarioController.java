39cb9de40ed2704d25947a05bac3d4b9e1a98418
Fabriciovics <fabriciovics@gmail.com>
2025-05-27 11:45:23 -0300

    Deploy do Projeto Blog Pessoal

--- src/main/java/com/genation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/com/genation/blogpessoal/controller/UsuarioController.java
@@ -1,17 +1,29 @@
 package com.genation.blogpessoal.controller;
 
-import com.genation.blogpessoal.model.Usuario;
-import com.genation.blogpessoal.model.UsuarioLogin;
-import com.genation.blogpessoal.repository.UsuarioRepository;
-import com.genation.blogpessoal.service.UsuarioService;
-import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
-import java.util.Optional;
+import com.genation.blogpessoal.model.Usuario;
+import com.genation.blogpessoal.model.UsuarioLogin;
+import com.genation.blogpessoal.repository.TemaRepository;
+import com.genation.blogpessoal.service.UsuarioService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -21,42 +33,37 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+    @GetMapping("/listar")
+    public ResponseEntity<List<Usuario>> listarTodos() {
 
-    @GetMapping
-    public ResponseEntity<List<Usuario>> getAll() {
         return ResponseEntity.ok(usuarioService.listarTodos());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("listar/{id}")
     public ResponseEntity<Usuario> getById(@PathVariable Long id) {
-        Optional<Usuario> usuario = usuarioService.buscarPorId(id);
-            return usuario.map(ResponseEntity::ok)
+        return usuarioService.listarPorId(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
-
     @PostMapping("/cadastrar")
     public ResponseEntity<Usuario> post(@Valid @RequestBody Usuario usuario) {
-        return  usuarioService.cadastrarUsuario(usuario)
+        return usuarioService.cadastrarUsuario(usuario)
                 .map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
                 .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
     }
 
     @PutMapping("/atualizar")
     public ResponseEntity<Usuario> put(@Valid @RequestBody Usuario usuario) {
-            return  usuarioService.atualizarUsuario(usuario)
-                    .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
-                    .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        return usuarioService.atualizarUsuario(usuario)
+                .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
     }
 
     @PostMapping("/logar")
-    public ResponseEntity<UsuarioLogin> autenticar(@Valid @RequestBody Optional<UsuarioLogin> usuarioLogin) {
-        return  usuarioService.autenticarUsuario(usuarioLogin)
-                .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+    public ResponseEntity<UsuarioLogin> post(@Valid @RequestBody Optional<UsuarioLogin> usuarioLogin) {
+        return usuarioService.autenticarUsuario(usuarioLogin)
+                .map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
                 .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
-
-
 }
--- src/main/java/com/genation/blogpessoal/service/UsuarioService.java
+++ src/main/java/com/genation/blogpessoal/service/UsuarioService.java
@@ -1,9 +1,8 @@
 package com.genation.blogpessoal.service;
 
-import com.genation.blogpessoal.model.Usuario;
-import com.genation.blogpessoal.model.UsuarioLogin;
-import com.genation.blogpessoal.repository.UsuarioRepository;
-import com.genation.blogpessoal.security.JwtService;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,8 +10,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import com.genation.blogpessoal.model.Usuario;
+import com.genation.blogpessoal.model.UsuarioLogin;
+import com.genation.blogpessoal.repository.UsuarioRepository;
+import com.genation.blogpessoal.security.JwtService;
 
 @Service
 public class UsuarioService {
@@ -26,51 +27,64 @@ public class UsuarioService {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    public List<Usuario> listarTodos() {
+        return usuarioRepository.findAll();
+    }
 
-    public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
-        if(usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent()){
+    public Optional<Usuario> listarPorId(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public Optional<Usuario> cadastrarUsuario(Usuario usuario){
+
+        if(usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent()) {
             return Optional.empty();
         }
+
         usuario.setSenha(criptografarSenha(usuario.getSenha()));
 
         return Optional.ofNullable(usuarioRepository.save(usuario));
     }
 
-    public Optional<Usuario> atualizarUsuario(Usuario usuario) {
+    public Optional<Usuario> atualizarUsuario(Usuario usuario){
 
-        if(usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent()) {
-            usuario.setSenha(criptografarSenha(usuario.getSenha()));
+        if(usuarioRepository.findById(usuario.getId()).isPresent()) {
 
-            return Optional.ofNullable(usuarioRepository.save(usuario));
+            Long usuarioPresenteId = usuarioRepository.findByUsuario(usuario.getUsuario()).get().getId();
+            if(usuarioPresenteId == usuario.getId()) {
+                usuario.setSenha(criptografarSenha(usuario.getSenha()));
+
+                return Optional.ofNullable(usuarioRepository.save(usuario));
+            }
+
         }
+
         return Optional.empty();
     }
 
-    public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin) {
-        var credencias = new UsernamePasswordAuthenticationToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha());
+    public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin){
 
-        Authentication authentication = authenticationManager.authenticate(credencias);
+        var credenciais = new UsernamePasswordAuthenticationToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha());
 
-        if(authentication.isAuthenticated()){
+        Authentication authentication = authenticationManager.authenticate(credenciais);
+
+        if(authentication.isAuthenticated()) {
             Optional<Usuario> usuario = usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
-            if(usuario.isPresent()){
+
+            if(usuario.isPresent()) {
+
                 usuarioLogin.get().setId(usuario.get().getId());
                 usuarioLogin.get().setNome(usuario.get().getNome());
                 usuarioLogin.get().setFoto(usuario.get().getFoto());
                 usuarioLogin.get().setSenha("");
                 usuarioLogin.get().setToken(gerarToken(usuarioLogin.get().getUsuario()));
+
                 return usuarioLogin;
+
             }
         }
+
         return Optional.empty();
-    }
-
-    public List<Usuario> listarTodos() {
-        return usuarioRepository.findAll();
-    }
-
-    public Optional<Usuario> buscarPorId(Long id) {
-        return usuarioRepository.findById(id);
     }
 
     private String gerarToken(String usuario) {
