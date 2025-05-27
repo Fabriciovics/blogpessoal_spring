e9bf8ca506068838e6966ce4e2fee51af7dea2dc
Fabriciovics <fabriciovics@gmail.com>
2025-05-27 16:02:54 -0300

    add dependency

--- src/main/java/com/genation/blogpessoal/BlogpessoalApplication.java
+++ src/main/java/com/genation/blogpessoal/BlogpessoalApplication.java
@@ -2,12 +2,8 @@ package com.genation.blogpessoal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.genation.blogpessoal")
-@EnableJpaRepositories(basePackages = "com.genation.blogpessoal.repository")
-@EntityScan(basePackages = "com.genation.blogpessoal.model")
+@SpringBootApplication
 public class BlogpessoalApplication {
 
 	public static void main(String[] args) {
