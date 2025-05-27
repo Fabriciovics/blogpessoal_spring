8023539a2121a209cc343ef036ec3b5a2baca973
Fabriciovics <fabriciovics@gmail.com>
2025-05-27 15:44:47 -0300

    change annotations in main

--- src/main/java/com/genation/blogpessoal/BlogpessoalApplication.java
+++ src/main/java/com/genation/blogpessoal/BlogpessoalApplication.java
@@ -2,8 +2,12 @@ package com.genation.blogpessoal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.genation.blogpessoal")
+@EnableJpaRepositories(basePackages = "com.genation.blogpessoal.repository")
+@EntityScan(basePackages = "com.genation.blogpessoal.model")
 public class BlogpessoalApplication {
 
 	public static void main(String[] args) {
